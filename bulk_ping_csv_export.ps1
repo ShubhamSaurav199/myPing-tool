87d985c822634d1eedd4faaadb076c885f2943d6
Shubham <shubhamsaurav199@gmail.com>
2023-04-27 14:25:55 +0530

    first commit

---
+++ bulk_ping_csv_export.ps1
@@ -0,0 +1,25 @@
+﻿$Output= @()
+
+$names = Get-Content "D:\IP-List\TB-SW.txt"
+
+foreach ($name in $names){
+
+if (Test-Connection -Delay 15 -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
+
+$Output+= "$name","Up"
+
+Write-Host "$Name,Up" -ForegroundColor Green
+
+}
+
+else{
+
+$Output+= "$name,Down"
+
+Write-Host "$Name,Down" -ForegroundColor Red
+
+}
+
+}
+
+$Output | Out-file "D:\OneDrive - L&T Construction\Commands\out.csv"
---
+++ thiru_ping.ps1
@@ -0,0 +1,23 @@
+﻿$Output = @()
+
+$names = Get-Content "D:\OneDrive - L&T Construction\Ping_IP_List\Wireless.txt"
+
+foreach ($name in $names) {
+    if (Test-Connection -Delay 1 -ComputerName $name -Count 1 -ErrorAction SilentlyContinue) {
+        $Output += [PSCustomObject] @{
+            ComputerName = $name
+            Status = "Up"
+        }
+        Write-Host "$Name,Up" -ForegroundColor Green
+        # !! execute ping -a IP.
+    }
+    else {
+        $Output += [PSCustomObject] @{
+            ComputerName = $name
+            Status = "Down"
+        }
+        Write-Host "$Name,Down" -ForegroundColor Red
+    }
+}
+
+$Output | Select-Object ComputerName, Status  | export-csv -path "D:\OneDrive - L&T Construction\Commands\out_wireless.csv"
